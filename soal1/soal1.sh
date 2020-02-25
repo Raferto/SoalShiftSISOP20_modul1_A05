8bbe7ef7ea9a35c841ffb026359ce9fd9cdad88e
Intan Kusuma R <61036923+S1A4C1D2@users.noreply.github.com>
2020-02-25 19:48:01 +0700

    Update soal1.sh

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -11,7 +11,7 @@ echo "$state"
 
 echo -e "\n1c"
 echo -e "Produk dengan profit paling sedikit:"
-st1=$(echo $state | sed -ne '1p')
-st2=$(echo $state | sed -ne '2p')
+st1=$(echo "$state" | sed -ne '1p')
+st2=$(echo "$state" | sed -ne '2p')
 produk=$(awk -F  "\t" -v st1="$st1" -v st2="$st2" '($11~st1) || ($11~st2) {arr[$17]+=$21} END {for(i in arr) print arr[i],i}' no1.tsv | sort -gk1 | head -10 | awk '{for(i=2;i<NF;i++) printf "%s", $i OFS; printf "%s", $NF ORS}')
 echo -e "$produk\n"
