cf42aa1b645c21a03d1c5cf1f65c896dc925dd54
Raferto <57932819+Raferto@users.noreply.github.com>
2020-02-29 20:24:26 +0700

    Update Soal3a.sh

--- soal3/Soal3a.sh
+++ soal3/Soal3a.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-
 wget -o "$(pwd)/Foto/wget.log"  "https://loremflickr.com/320/240/cat" -O "$(pwd)/Foto/pdkt_kusuma_1"
-
 
 for (( i = 2; i < 29; i++ )); do
   wget -a "$(pwd)/Foto/wget.log"  "https://loremflickr.com/320/240/cat" -O "$(pwd)/Foto/pdkt_kusuma_${i}"
